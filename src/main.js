45ee7f5e4b780227b622cb1fe318e3637ed0e105
Philippe Chapdelaine <philippe.chapdelaine@panevo.com>
2025-05-22 14:32:26 -0700

    fix: Resolve paths

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
 
-// Import Vue Flow styles
-import '@vue-flow/core/dist/style.css'
-import '@vue-flow/core/dist/theme-default.css'
-import '@vue-flow/controls/dist/style.css'
-import '@vue-flow/minimap/dist/style.css'
-import '@vue-flow/node-resizer/dist/style.css'
+// Import Vue Flow styles - using relative paths from node_modules
+import '../node_modules/@vue-flow/core/dist/style.css'
+import '../node_modules/@vue-flow/core/dist/theme-default.css'
+import '../node_modules/@vue-flow/controls/dist/style.css'
+import '../node_modules/@vue-flow/minimap/dist/style.css'
+import '../node_modules/@vue-flow/node-resizer/dist/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
